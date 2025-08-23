2bba918122a4331dd34b55d6bbc8d182196f45d1
101world <devnull@101world.com>
2025-08-23 07:40:53 +0530

    feat: Add required layout files for news system navigation

---
+++ app/social-twin/layout.tsx
@@ -0,0 +1,11 @@
+export default function SocialTwinLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
